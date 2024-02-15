ec550addf00036859e24a815d78d560b7c5db933
Devin P. O'Brien <devin.obrien@wbdcontractor.com>
2024-02-15 15:39:27 -0500

    Update ProjectModal.tsx

--- src/pages/ProductionApps/ProjectModal.tsx
+++ src/pages/ProductionApps/ProjectModal.tsx
@@ -16,9 +16,9 @@ export const ProjectModal = ({ project, closeModal }: ProjectModalProps) => {
   return (  
     <div className="fixed left-0 top-0 z-[1000] h-screen w-screen overflow-y-auto bg-gray-700/50 p-8 backdrop-blur-sm">
       <div
-        className='relative mx-auto h-[80vh] w-full overflow-y-auto rounded-lg bg-white px-8 shadow-lg dark:bg-slate-800 md:w-[50%]'
+        className='relative mx-auto h-[80vh] w-full overflow-y-auto rounded-lg bg-white shadow-lg dark:bg-slate-800 md:w-[50%]'
       >
-        <div className='sticky top-0 z-[1001] bg-white pt-8 dark:bg-slate-800'>
+        <div className='sticky top-0 z-[1001] bg-white px-8 pt-8 dark:bg-slate-800'>
           <button className="absolute right-0 top-0 p-4" onClick={() => closeModal()}>
             <Icon icon="ic:round-close" className="size-8 dark:text-white"/>
           </button>
@@ -28,45 +28,48 @@ export const ProjectModal = ({ project, closeModal }: ProjectModalProps) => {
           </SubTitle>
           <hr className="my-2"/>
         </div>
-        <TextSection>
-          {description}
-        </TextSection>
-        <hr className="my-2"/>
-        {
-          image_url && (
-            <div>
-              <h2 className="font-montserrat text-xl dark:text-white">Website Link</h2>
-              <a 
-                className="block justify-center overflow-hidden py-4 transition-all hover:scale-105 md:w-1/2"
-                href={url}
-                target="_blank"
-              >
-                <img src={image_url} alt={title} className="rounded-t-lg object-cover shadow"/>
-                <p
-                  className='cursor-pointer rounded-b-lg bg-azure-blue p-2 text-center font-montserrat text-charcoal shadow'
+
+        <div className='px-8'>
+          <TextSection>
+            {description}
+          </TextSection>
+          <hr className="my-2"/>
+          {
+            image_url && (
+              <div>
+                <h2 className="font-montserrat text-xl dark:text-white">Website Link</h2>
+                <a 
+                  className="block justify-center overflow-hidden py-4 transition-all hover:scale-105 md:w-1/2"
+                  href={url}
+                  target="_blank"
                 >
+                  <img src={image_url} alt={title} className="rounded-t-lg object-cover shadow"/>
+                  <p
+                    className='cursor-pointer rounded-b-lg bg-azure-blue p-2 text-center font-montserrat text-charcoal shadow'
+                  >
                 click to visit {title}
-                </p>
-              </a>
-              <hr className="my-2"/>
-            </div>
-          )
-        }
-        <div className="">
-          <h2 className="font-montserrat text-xl dark:text-white">Skills Required</h2>
-          {skills?.map((skill) => {
-            return (
-              <div key={skill.title} className="mx-auto my-2 flex gap-2">
-                <div>
-                  <Icon icon={skill.icon} className="size-20 rounded bg-white p-1 shadow"/>
-                </div>
-                <div>
-                  <p className="my-auto font-montserrat text-xl dark:text-white ">{skill.title}</p>
-                  <p className="my-auto font-montserrat text-sm dark:text-slate-300 ">{skill.description}</p>
-                </div>
+                  </p>
+                </a>
+                <hr className="my-2"/>
               </div>
-            );
-          })}
+            )
+          }
+          <div className="">
+            <h2 className="font-montserrat text-xl dark:text-white">Skills Required</h2>
+            {skills?.map((skill) => {
+              return (
+                <div key={skill.title} className="mx-auto my-2 flex gap-2">
+                  <div>
+                    <Icon icon={skill.icon} className="size-20 rounded bg-white p-1 shadow"/>
+                  </div>
+                  <div>
+                    <p className="my-auto font-montserrat text-xl dark:text-white ">{skill.title}</p>
+                    <p className="my-auto font-montserrat text-sm dark:text-slate-300 ">{skill.description}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
